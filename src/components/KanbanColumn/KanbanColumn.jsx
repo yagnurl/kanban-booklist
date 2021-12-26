5b2dc7343a213c7b52e48ade71c14fe8dee54d44
Yağnur Tetikcan <50737107+yagnurl@users.noreply.github.com>
2021-12-26 16:13:27 +0300

    bugs fixed

--- src/components/KanbanColumn/KanbanColumn.jsx
+++ src/components/KanbanColumn/KanbanColumn.jsx
@@ -1,53 +1,45 @@
-import React from 'react'
-import ColumnItem from '../ColumnItem/ColumnItem'
-import './style.scss'
+import React from "react";
+import ColumnItem from "../ColumnItem/ColumnItem";
+import "./style.scss";
 export default function KanbanColumn(props) {
-
-    //`props.books` is an array of objects, `key` is the key to group by
-    let groupByYear = props.books.reduce((storage, item) => { 
+  //`props.books` is an array of objects, `key` is the key to group by
+  let groupByYear = props.books.reduce((storage, item) => {
     // set `storage` for the instance
-    storage[item.first_publish_year] = storage[item.first_publish_year] || []; 
+    storage[item.first_publish_year] = storage[item.first_publish_year] || [];
+
     // add this item to its group within `storage`
-    storage[item.first_publish_year].push(item); 
-    // return the updated storage 
-    return storage; 
-  }, Object.create(null)); //  initial value of the storage    
+    storage[item.first_publish_year].push(item);
 
-  
-    // convert groupByYear object into key's array to make iterable
-    const keys = Object.keys(groupByYear);
-    let values = []
+    // return the updated storage
+    return storage;
+  }, Object.create(null)); //  initial value of the storage
 
+  // convert groupByYear object into key's array to make iterable
+  const keys = Object.keys(groupByYear);
+  let values = [];
 
-    // iterate the keys to determine column id's (publish year)
-    Object.keys(groupByYear).forEach((key) => {
-        values.push(groupByYear[key])
-        console.log("values", values);
-    })
-    
-    let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+  // iterate the keys to determine column id's (publish year)
+  Object.keys(groupByYear).forEach((key) => {
+    values.push(groupByYear[key]);
+    // console.log("values", values);
+  });
 
-    return (
-        <div className="kanbanContainer">
-            {
-                keys
-                .filter((key) => key !== typeof(undefined))
-                .map((key) => (
-                    <div className="kanbanColumn" id={key} style={{borderTopColor: randomColor}}>
-                        <p className="fs-20">{key}</p>
-                        {values
-                         
-                         .map((item) => item
-                         .map((element) =>
-                            key == element.first_publish_year ? (
-                            <ColumnItem item={item} props={element} />
-                            ) : null)
-                            
-                            )
-                        }
-                    </div>
-                ))
-            }
-        </div>
-    );
+  return (
+    <div className="kanbanContainer">
+      {keys
+        .filter((key) => key !== typeof undefined)
+        .map((key) => (
+          <div className="kanbanColumn" id={key}>
+            <p className="fs-20">{key}</p>
+            {values.map((item) =>
+              item.map((element) =>
+                key == element.first_publish_year ? (
+                  <ColumnItem item={item} props={element} />
+                ) : null
+              )
+            )}
+          </div>
+        ))}
+    </div>
+  );
 }
